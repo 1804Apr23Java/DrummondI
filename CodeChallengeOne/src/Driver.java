d527c6fb2c4b38519da30661a90f99a817a60d24
ianH92 <ianhdrummond1992@gmail.com>
2018-04-30 07:57:47 -0700

    Completed program working

--- CodeChallengeOne/src/Driver.java
+++ CodeChallengeOne/src/Driver.java
@@ -6,10 +6,12 @@ public class Driver {
 	public static void main(String[] args) {
 		LinkedList<String> sharedResource = new LinkedList<>();
 		Lock lock = new Lock();
-		final int MAX_LEN = 100;
+		final int MAX_LEN = 5000;
 		
 		Producer p = new Producer(sharedResource, MAX_LEN, lock);
 		Consumer c = new Consumer(sharedResource, MAX_LEN, lock);
-		System.out.println("Done");
+	
+		(new Thread(new Producer(sharedResource, MAX_LEN, lock))).start();
+		(new Thread(new Consumer(sharedResource, MAX_LEN, lock))).start();
 	}
 }
