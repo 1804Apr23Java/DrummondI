1160eab0de662ebbfbe5816910ec400d09359963
ianH92 <ianhdrummond1992@gmail.com>
2018-05-10 10:59:47 -0700

    Testing for EmployeeDao Started

--- project1/project1/src/test/java/com/revature/project1/dao/EmployeeDaoTest.java
+++ project1/project1/src/test/java/com/revature/project1/dao/EmployeeDaoTest.java
@@ -1,14 +1,52 @@
 package com.revature.project1.dao;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.sql.SQLException;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.revature.project1.transportObjects.Employee;
 
 public class EmployeeDaoTest {
 	
-	@Test
-	public void testOne() {
-		assertTrue(true);
+	private static final EmployeeDao e = new EmployeeDao();
+	
+	@Before
+	public void setupDatabaseForTests() {
+		
 	}
+	
+	@Test
+	public void testGetEmployeeById() throws SQLException {
+		assertEquals(1, e.getEmployeeById(1).getEmployeeId());
+	}
+	
+	@Test
+	public void testGetEmployeeByIdValidUsername() throws SQLException {
+		Employee em = e.getEmployeeById(1);
+		assertTrue(em.getUsername().equals("Username") 
+				   && em.getFirstname().equals("Firstname") 
+				   && em.getLastname().equals("Lastname") 
+				   && em.getEmail().equals("Email") 
+				   && em.getPassword().equals("Password"));
+	}
+	
+	@Test
+	public void testGetEmployeeByUsername() throws SQLException {
+		assertEquals(1, e.getEmployeeByUsername("Username").getEmployeeId());
+	}
+	
+	@Test
+	public void testGetEmployeeByUsernameValidFields() throws SQLException {
+		Employee em = e.getEmployeeByUsername("Username");
+		assertTrue(em.getUsername().equals("Username") 
+				   && em.getFirstname().equals("Firstname") 
+				   && em.getLastname().equals("Lastname") 
+				   && em.getEmail().equals("Email") 
+				   && em.getPassword().equals("Password"));
+	}	
 }
