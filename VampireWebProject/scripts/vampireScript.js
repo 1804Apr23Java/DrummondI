a3b04fb910eec2e505ccba653eb9994708da18db
Collin Meaney <collinmeaney375@gmail.com>
2018-05-11 15:17:00 -0400

    Modified the eventListener to contain name as well

--- VampireWebProject/scripts/vampireScript.js
+++ VampireWebProject/scripts/vampireScript.js
@@ -1,6 +1,6 @@
 let submitForm = document.getElementById("submitForm");
 submitForm.addEventListener("click", function(){
     let p = document.createElement('P');
-    p.innerHTML = document.getElementById("commentBox").value;
+    p.innerHTML = document.getElementById("inputName").value+' : ' +document.getElementById("commentBox").value;
     document.getElementById("outputForm").appendChild(p);
 });
