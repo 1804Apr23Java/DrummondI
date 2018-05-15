92153802f4a7cbc751fc6cdefe0b45bc6a7da029
ianH92 <ianhdrummond1992@gmail.com>
2018-05-15 11:46:15 -0700

    created request table

--- project1/project1TableCreationScript.sql
+++ project1/project1TableCreationScript.sql
@@ -23,5 +23,31 @@ BEGIN
     SELECT inc.NEXTVAL INTO :NEW.e_id FROM DUAL;
 END;
 
-INSERT INTO all_emp (e_un, e_pw, e_em, e_fn, e_ln) VALUES ('Username', 'Password', 'Email', 'Firstname', 'Lastname');
+COMMIT;
+
+--///////////////////////////////////////////////////////
+CREATE TABLE requests(
+    r_id INTEGER PRIMARY KEY,
+    e_id INTEGER NOT NULL,
+    r_date DATE DEFAULT CURRENT_DATE,
+    r_amt NUMBER(14,2),
+    r_img BLOB,
+    r_stat INTEGER DEFAULT 1,
+    CONSTRAINT fk_e_id FOREIGN KEY (e_id) REFERENCES all_emp(e_id),
+    CONSTRAINT r_stat_valid CHECK (r_stat = 1 OR r_stat = 2 OR r_stat = 3)
+);
+/
+
+CREATE SEQUENCE requests_seq
+    MINVALUE 1
+    START WITH 1
+    INCREMENT BY 1;
+    
+CREATE OR REPLACE TRIGGER requests_pk
+    BEFORE INSERT ON requests
+    FOR EACH ROW
+BEGIN
+    SELECT requests_seq.NEXTVAL INTO :NEW.r_id FROM DUAL;
+END;
+
 COMMIT;
